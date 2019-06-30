7bb3610e2f95cc405ebbcf3f8c04f4c3a6e310f6
Ian McNair <imcnair13@gmail.com>
2019-06-29 23:01:54 -0400

    making sure files are updated

--- democratic_wordclouds.py
+++ democratic_wordclouds.py
@@ -1,45 +1,59 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jun 28 22:22:43 2019
-
-@author: imcna
-"""
 import numpy as np
 import pandas as pd
 from PIL import Image
-from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
-
+from wordcloud import WordCloud, ImageColorGenerator
+import nltk
+from nltk.corpus import stopwords as sw
 import matplotlib.pyplot as plt
-
 import warnings
 warnings.filterwarnings("ignore")
-#
-df = pd.read_excel('candidates1.xlsx')
+
+nltk.download('stopwords')
+
+df = pd.read_excel('candidates.xlsx')
 df['speech_upper'] = df.speech.str.upper()
 
-import nltk
-nltk.download('stopwords')
-from nltk.corpus import stopwords as sw
-sw.words('english')
-
-
-speech = df.speech_upper[10]
-
+sns.set_context('poster')
 mask = np.array(Image.open('flag.jpg'))
 
-wordcloud = WordCloud(stopwords=sw.words('english'), 
-                          background_color="white", 
-                          mode="RGBA", 
-                          max_words=2000, 
-                          mask=mask).generate(speech)
+for i in range(len(df.candidates)):
+    """
+    This loop grabs the speech and cndidate name.
+    Then it creates a word cloud and saves it.
+    Unfortunately, I haven't figured out how to not display the images
+    ,but it at least works...
+    """
+    speech = df.speech_upper[i]
+    name = df.candidates[i]
+        
+    wordcloud = WordCloud(stopwords=sw.words('english'), 
+                              background_color="white", 
+                              mode="RGBA", 
+                              max_words=1000, 
+                              mask=mask).generate(speech)
+    
+    # create coloring from image
+    image_colors = ImageColorGenerator(mask)
+    
+    plt.figure(figsize=[15,10])
+    plt.imshow(wordcloud.recolor(color_func=image_colors), 
+               interpolation="bilinear")
+    plt.axis("off")
+    plt.title(name)
+    plt.savefig(name +'.png', format = 'png')
 
-# create coloring from image
-image_colors = ImageColorGenerator(mask)
 
-#wordcloud_usa.to_file('usacloud.jpg')
-plt.figure(figsize=[20,10])
-plt.imshow(wordcloud.recolor(color_func=image_colors), 
-           interpolation="bilinear")
-plt.axis("off")
-plt.savefig('wordcloud.png', format = 'png')
-plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
